a8c820fe54aa432d652be5f6fca6d4d067cb9bc3
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-09-22 13:53:03 +0530

    v2.3.1 responsive

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -9,37 +9,37 @@ function Services() {
 
   const servicesData = [
     {
-      img: "/Services/Services1.png",
+      img: "/Services/Services7.svg",
       heading: "Business Incorporation",
       description:
         "Get company incorporation assistance and compliance support right after the registration process. Tell us your requirements and let us drive your investment and growth with business incorporation services",
     },
     {
-      img: "/Services/Services2.png",
+      img: "/Services/Services6.png",
       heading: "Business Consultation",
       description:
         "Our business consultation services augment your business requirements through essential aspects of businesses like people, processes, technology, and related operations.",
     },
     {
-      img: "/Services/Services3.png",
+      img: "/Services/Services8.svg",
       heading: "Financial & Accounting",
       description:
         "Through the regular management of financial reports, accounting, and related activities of the business, we are involved in the analysis and reporting of financial processes.",
     },
     {
-      img: "/Services/Services4.png",
+      img: "/Services/Services9.svg",
       heading: "Audit & Taxation",
       description:
         "We engage in the routine monitoring, review, and report of the significant tax matters that impact your business with the panel of our audit and tax experts.",
     },
     {
-      img: "/Services/Services5.png",
+      img: "/Services/Services10.svg",
       heading: "technology solutions",
       description:
         "Along with the business experience, we infuse technology into the business processes to address your specific technical needs that reimagine business models.",
     },
     {
-      img: "/Services/Services6.png",
+      img: "/Services/Services11.svg",
       heading: "digital marketing",
       description:
         "Hand over all your digital marketing to-do tasks to our experts and watch through the growth plateau with the finest services that scale your brand outreach.",
--- src/app/components/WhyBeacon.js
+++ src/app/components/WhyBeacon.js
@@ -4,37 +4,37 @@ import React from "react";
 function WhyBeacon() {
   const beaconData = [
     {
-      img: "/BeaconIcon/BeaconIcon1.png",
+      img: "/BeaconIcon/BeaconIcon7.svg",
       heading: "Customer Centric",
       description:
         "We understand the customers and consider their expectations to align the services with their needs.",
     },
     {
-      img: "/BeaconIcon/BeaconIcon2.png",
+      img: "/BeaconIcon/BeaconIcon8.svg",
       heading: "global perspective",
       description:
         "Our team considers the business concerns and resolves them while addressing them on a global level.",
     },
     {
-      img: "/BeaconIcon/BeaconIcon3.png",
+      img: "/BeaconIcon/BeaconIcon9.svg",
       heading: "Collaborative Approach",
       description:
         "With collaboration, we equip businesses to face real-world challenges and retain relationships.",
     },
     {
-      img: "/BeaconIcon/BeaconIcon4.png",
+      img: "/BeaconIcon/BeaconIcon10.svg",
       heading: "Communication",
       description:
         "We adopt the process of sharing ideas and developing solutions based on the client's needs.",
     },
     {
-      img: "/BeaconIcon/BeaconIcon5.png",
+      img: "/BeaconIcon/BeaconIcon11.svg",
       heading: "Dedicated Teams",
       description:
         "Our team dedicatedly works on project realization right from the beginning to the final deployment.",
     },
     {
-      img: "/BeaconIcon/BeaconIcon6.png",
+      img: "/BeaconIcon/BeaconIcon12.svg",
       heading: "Ease of Setup",
       description:
         "With us, your business setup is never going to be a hassle— it becomes as easy as a pie!",
