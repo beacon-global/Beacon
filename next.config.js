17cdb73c6aabb4a9236b8cc4c6f01790349f27e7
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-10-03 22:32:12 +0530

    v3.1.1 svgChangeEdits

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats: ['image/jpg', 'image/png', 'image/svg'], // Add any other desired formats
-        quality: 100, // Adjust the quality value as needed (0-100)
-      },
+    // images: {
+    //     formats: ['image/jpg', 'image/png', 'image/svg'], // Add any other desired formats
+    //     quality: 100, // Adjust the quality value as needed (0-100)
+    //   },
 }
 
 module.exports = nextConfig
