7d0ce710f77ae80a87bc775615a70b5befa03ed9
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-10-10 22:26:32 +0530

    v3.4.4 metaData

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Beacon - Your Global Advisory Partner For Business Success",
-  description: "Beacon is your global business advisory partner who offers you a range of services that enable your business growth and ultimate business transformation. We provide a plethora of services like Business Incorporation, Digital Marketing, Technology, Finance and Accounting, Audit & Taxation and Business Consulting services.",
+  description:
+    "Beacon is your global business advisory partner who offers you a range of services that enable your business growth and ultimate business transformation. We provide a plethora of services like Business Incorporation, Digital Marketing, Technology, Finance and Accounting, Audit & Taxation and Business Consulting services.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/GREEN.svg" type="image/svg+xml" />
+        <link
+          rel="icon"
+          href="/GREEN.svg"
+          type="image/svg+xml"
+          sizes="16x16 32x32 48x48"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
