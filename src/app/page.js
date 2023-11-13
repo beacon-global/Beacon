63b8f22f3177924d3577b76891c61c80015ebac4
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-11-13 18:33:57 +0530

    v3.5.10 loadingAnimationChange

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
       if (whiteScreen) {
         whiteScreen.classList.add("hidden");
       }
-    }, 2500); // 1000 milliseconds = 1 second
+    }, 250000000); // 1000 milliseconds = 1 second
 
     // Cleanup the timeout to avoid memory leaks
     return () => clearTimeout(timeoutId);
