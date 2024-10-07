d78e79245911f4da4f5f18f3e59985d7f4b671dc
lazim-mv <lazimlatheef@gmail.com>
2024-10-07 14:30:49 +0530

    keywords

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,36 @@ export const metadata = {
   title: "Beacon - Your Global Advisory Partner For Business Success",
   description:
     "Beacon is your global business advisory partner who offers you a range of services that enable your business growth and ultimate business transformation. We provide a plethora of services like Business Incorporation, Digital Marketing, Technology, Finance and Accounting, Audit & Taxation and Business Consulting services.",
+  keywords: [
+    "business consulting",
+    "management consulting",
+    "consulting companies",
+    "financial consulting",
+    "business management consulting",
+    "company formation in saudi arabia",
+    "saudi company formation",
+    "registering a company in saudi arabia",
+    "consulting firms in saudi arabia",
+    "business consultants in saudi arabia",
+  ],
   openGraph: {
     title: "Beacon - Your Global Advisory Partner For Business Success",
     description:
       "Beacon is your global business advisory partner who offers you a range of services that enable your business growth and ultimate business transformation. We provide a plethora of services like Business Incorporation, Digital Marketing, Technology, Finance and Accounting, Audit & Taxation and Business Consulting services.",
     url: "https://bmcglobal.co",
     siteName: "Beacon",
+    keywords: [
+      "business consulting",
+      "management consulting",
+      "consulting companies",
+      "financial consulting",
+      "business management consulting",
+      "company formation in saudi arabia",
+      "saudi company formation",
+      "registering a company in saudi arabia",
+      "consulting firms in saudi arabia",
+      "business consultants in saudi arabia",
+    ],
     images: [
       {
         url: "https://bmcglobal.co/linkImageWhatsApp.png",
