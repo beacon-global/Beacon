4321d394d25ae4f7562c553bb6dbfcd2846a5a5c
lazim-mv <lazimlatheef@gmail.com>
2024-10-07 14:43:50 +0530

    keywords

--- src/app/pages/blog/page.js
+++ src/app/pages/blog/page.js
@@ -6,18 +6,7 @@ import Footer from "@/app/components/Footer";
 import Contact from "@/app/components/Contact";
 import styles from "./blogs.module.css";
 
-export const metadata = {
-  keywords: [
-    "Beacon",
-    "business advisory",
-    "business growth",
-    "digital marketing",
-    "technology services",
-    "finance and accounting",
-    "audit and taxation",
-    "business consulting",
-  ],
-};
+
 
 const page = () => {
   return (
